b5b74cbeb7b438f483f2a71f009ff9f1dd9676ba
Ernestas <34349429+ErnestasBakucionis@users.noreply.github.com>
2022-08-13 13:55:36 +0300

    added Semantic UI Was a problem with sematic.css lib file. (double ';;' symbols in lib file, due to semantic UI abandoned development )

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+
+import MenuBar from "./components/MenuBar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <div><h1>This is a social media app</h1></div>
+    <BrowserRouter>
+      <Container>
+        <MenuBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
   );
 }
 
