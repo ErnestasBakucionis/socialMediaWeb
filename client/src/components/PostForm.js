1a952f7eb6e206ca750a51a3cfe560263564c4ca
Ernestas <ernysbaku3@gmail.com>
2022-08-17 10:39:05 +0300

    Added error message for creating post and transition animation for created post

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -30,6 +30,7 @@ function PostForm() {
   }
 
   return (
+    <>
     <Form onSubmit={onSubmit}>
       <h2>Create a post:</h2>
       <Form.Field>
@@ -38,12 +39,21 @@ function PostForm() {
           name="body"
           onChange={onChange}
           value={values.body}
+          error={error ? true : false}
         />
         <Button type="submit" color="olive">
           Submit
         </Button>
       </Form.Field>
     </Form>
+    {error && (
+      <div className="ui error message" style={{marginBottom: 20}}>
+      <ul className="list">
+        <li>{error.graphQLErrors[0].message}</li>
+      </ul>
+      </div>
+    )}
+    </>
   );
 }
 
